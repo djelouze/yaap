9ecab5a553bbfb901102664917cfa6d31c90ecb9
Jerome Velut <jerome.velut@gmail.com>
2011-05-31 22:32:08 +0200

    Yet Another Argument Parser

---
+++ testyaap.cxx
@@ -0,0 +1,82 @@
+//! \file testyaap.cxx
+//! \brief Illustration of the Argument Parser yaap namespace
+//! 
+//! try with for instance:
+//!   [shell]$ testyaap -i inputfile.txt -vV -o outputfile.raw -s .558 .558 0.89
+
+// The only thing to do to define the yaap namespace
+#include "yaap.h"
+
+
+int main( int argc, char** argv )
+{
+
+////////////////// OPTION LAYOUT DEFINITION /////////////////////
+   // Initialize a parser following the command line argument
+   yaap::Parser parser( argc, argv );
+   
+   // Add an option of type "-i filename"
+   yaap::OptionArg<std::string,1>* inputOpt = parser.AddOptionArg<std::string,1>( 'i', "Input file (.vti)");
+   // Add an option of type "-e 0 127 0 127 0 127"
+   yaap::OptionArg<int,6>* extentOpt = parser.AddOptionArg<int,6>( 'e', "Extent (dimension): xmin xmax ymin ymax zmin zmax (integer)");
+   // Add an option of type "-s 0.588 0.588 0.877"
+   yaap::OptionArg<double,3>* spacingOpt = parser.AddOptionArg<double,3>( 's', "Spacing (size of pixel): x y z (double)");
+   // Add an option of type "-o filename"
+   yaap::OptionArg<std::string,1>* outputOpt = parser.AddOptionArg<std::string,1>( 'o', "Output file (.vti)");
+   // Add a simple option for ie. debugging information
+   yaap::Option* verboseOpt = parser.AddOption('v', "Verbose output");
+   // Add a simple option for ie. version display
+   yaap::Option* versionOpt = parser.AddOption('V', "Display version");
+
+//////////////// NOW THE OPTIONS ARE DEFINED. JUTS USE THEM //////   
+
+   if( !parser.IsCommandLineValid( ) )
+   {
+      parser.Usage( );
+      return( 0 );
+   }
+   
+   // Check verbose option
+   std::cout << "Verbose? "; 
+   (verboseOpt->Exists())? std::cout << "Yes": std::cout << "No";
+   std::cout << std::endl;
+
+   // Check version option
+   std::cout << "Version? "; 
+   (versionOpt->Exists())? std::cout << "Yes": std::cout << "No";
+   std::cout << std::endl;
+
+   // Check input filename option
+   std::cout << "Input filename: "; 
+   (inputOpt->Exists())? std::cout << inputOpt->GetArgument(0): std::cout << "n/a";
+   std::cout << std::endl;
+
+   // Check output filename option
+   std::cout << "Output filename: "; 
+   (outputOpt->Exists())? std::cout << outputOpt->GetArgument(0): std::cout << "n/a";
+   std::cout << std::endl;
+   
+   // Check extent option
+   std::cout << "Extent: ";
+   if(extentOpt->Exists())
+   {
+      for( int i = 0; i < 5; i++ )
+         std::cout << extentOpt->GetArgument(i) << " ; ";
+      std::cout << extentOpt->GetArgument(5) << std::endl;
+   }
+   else
+      std::cout << "n/a" << std::endl;
+
+   // Check spacing option
+   std::cout << "Spacing: ";
+   if(spacingOpt->Exists())
+   {
+      for( int i = 0; i < 2; i++ )
+         std::cout << spacingOpt->GetArgument(i) << " ; ";
+      std::cout << spacingOpt->GetArgument(2) << std::endl;
+   }
+   else
+      std::cout << "n/a" << std::endl;
+
+return ( 1 );
+}
---
+++ yaap.h
@@ -0,0 +1,230 @@
+// Copyright 2011 Jérôme Velut. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+//    1. Redistributions of source code must retain the above copyright notice, this list of
+//       conditions and the following disclaimer.
+//
+//    2. Redistributions in binary form must reproduce the above copyright notice, this list
+//       of conditions and the following disclaimer in the documentation and/or other materials
+//       provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY Jérôme Velut ``AS IS'' AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Jérôme Velut OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// The views and conclusions contained in the software and documentation are those of the
+// authors and should not be interpreted as representing official policies, either expressed
+// or implied, of Jérôme Velut.
+
+#ifndef __yaap_h__
+#define __yaap_h__
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+//! \namespace yaap contains the classes for command line arguments parsing
+namespace yaap {
+
+//! \class Option
+//! \brief Defines a boolean option
+//!
+//! An option is defined by its flag, a description and a state. The flag and
+//! and description are given at construction, whereas the state is defined
+//! after the effective parsing.
+class Option {
+public:
+    //! constructor.
+    Option( char flag, std::string description )
+    {
+        this->flag = flag;
+        this->description = description;
+        this->state = false;
+    };
+
+    //! Get the flag of this option.
+    char Flag( ) {
+        return( this->flag);
+    };
+    //! Set the state (exists or not in the command line)
+    void Exists(bool state) {
+        this->state = state;
+    };
+    //! Get the state. If true, the option exists in the command line
+    bool Exists() {
+        return(this->state);
+    };
+
+   //! Print how to use the option in the command line format
+   virtual void CLUsage( )
+   {
+      std::cout << " [-" << this->Flag() << "]";
+   }
+
+
+    std::string GetDescription( )
+    {return( this->description );};
+
+protected:
+    char flag; //!< Command line flag character
+    std::string description;//!< Short description
+    bool state;//!< true if present in the command line
+};
+
+//! \class OptionArg
+//! \brief Defines an option with possibly several arguments
+//!
+//! This is a templated class derived from Option. It adds a vector of
+//! arguments which type and number is defined by the template statements T
+//! and N.
+template<class T, int N>
+class OptionArg : public Option {
+
+public:
+    //! constructor
+    OptionArg( char flag, std::string description ):Option(flag,description)
+    {
+        this->nbArgs = N;
+    };
+
+    //! Set the pos-th arg of type T.
+    void SetArgument( T arg, int pos ) {
+        this->argVector[pos] = arg;
+    };
+    //! Get the pos-th arg of type T
+    T GetArgument( int pos ) {
+        return( argVector[pos] );
+    };
+
+   //! Print how to use the option in the command line format
+   virtual void CLUsage( )
+   {
+      std::cout << " [-" << this->Flag();
+      for( int i = 0; i < nbArgs; i++ )
+         std::cout << " x";
+      std::cout << "]"; 
+   }
+
+protected:
+    int nbArgs; //!< Number of arguments of this specific option
+    T argVector[N]; //!< Vector of arguments of type T
+};
+
+//! \class Parser
+//! \brief Manages a set of options
+class Parser {
+public:
+    //! constructor. Initializes number of args and argument vector.
+    Parser( int argc, char** argv )
+    {
+        this->nbArgs = argc;
+        this->argv = argv;
+        this->error = 0;
+    }
+
+    //! destructor
+    virtual ~Parser()
+    {
+        // todo: delete options
+    }
+
+    //! Add a simple option with given flag and description to the options
+    //! vector and check its existence.
+    //! \return the instanciated Option
+    Option* AddOption( char flag, std::string description )
+    {
+        Option* option = new Option( flag, description );
+
+        // For each argument in the command, check the underlying string
+        for( int i = 1; i < this->nbArgs; i++ )
+        {
+            if( this->argv[i][0] == '-' ) // This is an option(s) statement
+            {
+                // The option flag can be concatenated after a unique '-'
+                int c = 0;
+                while(argv[i][c] != '\0' )
+                {
+                    if( argv[i][c] == option->Flag() ) // It is the processed option
+                        option->Exists(true); // toggle the state in the Option object
+                    c++;
+                }
+            }
+        }
+        // Put the Option in the options' array.
+        this->optionVector.push_back( option );
+        // Return the created Option for the user to use it in the main program
+        return( option );
+    }
+
+    //! Add an option with arguments with given flag and description to the
+    //! options vector, check its existence and perform the sub-arguments
+    //! assignment. There are N sub-arguments of type T.
+    //! \return the instanciated OptionArg
+    template<class T, int N>
+    OptionArg<T,N>* AddOptionArg( char flag, std::string description )
+    {
+        // option allocation
+        OptionArg<T,N>* option = new OptionArg<T,N>( flag, description );
+
+        // parse the argument list
+        for( int i = 1; i < this->nbArgs; i++ )
+        {
+            if( this->argv[i][0] == '-' ) // This is an option(s) statement
+            {
+                if( argv[i][1] == option->Flag() ) // this is the processed option
+                {
+                    // toggle the state of the option to true
+                    option->Exists(true);
+
+                    for( int argIdx = 1; argIdx <= N ; argIdx++)
+                    {
+                        // For each sub-argument, memorize the command line value in
+                        // the OptionArg object
+                       std::istringstream argStream( argv[i+argIdx] );
+                        T arg;
+                        argStream >> arg;
+                        option->SetArgument( arg, argIdx-1 );
+                    }
+                }
+            }
+        }
+        // Put the Option in the options' array.
+        this->optionVector.push_back( option );
+        // Return the created Option for the user to use it in the main program
+        return( option );
+    }
+
+   void Usage( )
+   {
+      std::cout << "Usage: \n [shell]$ " << this->argv[0];
+      for( int i = 0; i < optionVector.size(); i++ )
+         optionVector[i]->CLUsage();
+      std::cout << std::endl;
+
+      for( int i = 0; i < optionVector.size(); i++ )
+         std::cout << "\t -" << optionVector[i]->Flag() << " : " << optionVector[i]->GetDescription() << std::endl;
+   } 
+
+   bool IsCommandLineValid( )
+   { return( !this->error );};
+
+private:
+    int nbArgs; //!< Number of arguments (argc)
+    char** argv; //!< Arguments' vector
+    std::vector<Option*> optionVector; //!< vector of options
+    bool error; //!< raised to 1 when one of the arguments in the command line is not valid
+};
+
+
+}; //end namespace yaap
+
+#endif //yaap
